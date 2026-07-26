c4b505c9adcfc0308f857b0a883c5360237abefa
michaelramirezradio <info@michaelramirez.pro>
2026-07-26 14:02:10 +0100

    ver2
    nueva cpsa

--- app.js
+++ app.js
@@ -32,15 +32,189 @@ var activePanelId = null;
 var isHdStreamPlaying = false;
 var currentMenuIndex = 0;
 
-// Lista de canciones para simulación de metadatos de radio en vivo
-var songPlaylist = [
-    { title: "Como Una Ola", artist: "Rocío Jurado", album: "Grandes Éxitos (1981)", cover: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500&q=80" },
-    { title: "Un Beso y Una Flor", artist: "Nino Bravo", album: "Un Beso y Una Flor (1972)", cover: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=500&q=80" },
-    { title: "Vivir Así Es Morir De Amor", artist: "Camilo Sesto", album: "Sentimientos (1978)", cover: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=500&q=80" },
-    { title: "Me Olvidé de Vivir", artist: "Julio Iglesias", album: "A mis 33 años (1977)", cover: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=500&q=80" },
-    { title: "La Puerta de Alcalá", artist: "Ana Belén y Víctor Manuel", album: "Para la Ternura (1986)", cover: "https://images.unsplash.com/photo-1465847899084-d164df4dedc6?w=500&q=80" }
+// Tabla de Programación Oficial de Inolvidable FM Canarias (Extraída de inolvidablefm.es/#programas)
+var officialWeeklySchedule = {
+    // 0 = Domingo, 1 = Lunes, 2 = Martes, 3 = Miércoles, 4 = Jueves, 5 = Viernes, 6 = Sábado
+    1: [ // LUNES
+        { start: "01:00", end: "05:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "05:00", end: "11:00", show: "Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "15:00", show: "Fórmula Inolvidable", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "15:00", end: "17:00", show: "Prime Time del Atasco", dj: "José Ibáñez", title: "Radio DJ & Conductor de El Prime Time", photo: "djs/jose_ibanez.jpg" },
+        { start: "17:00", end: "21:00", show: "Fórmula Inolvidable", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "21:00", end: "23:00", show: "Inolvidables de Colección", dj: "Carmen Díaz", title: "Radio DJ & Conductora de Colección", photo: "djs/carmen_diaz.jpg" },
+        { start: "23:00", end: "01:00", show: "Baladas Inolvidables", dj: "Emisión Automática", title: "Selección Nocturna de Baladas", photo: "logo.png" }
+    ],
+    2: [ // MARTES
+        { start: "01:00", end: "05:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "05:00", end: "11:00", show: "Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "15:00", show: "Fórmula Inolvidable", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "15:00", end: "17:00", show: "Prime Time del Atasco", dj: "José Ibáñez", title: "Radio DJ & Conductor de El Prime Time", photo: "djs/jose_ibanez.jpg" },
+        { start: "17:00", end: "21:00", show: "Fórmula Inolvidable", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "21:00", end: "23:00", show: "Inolvidables de Colección", dj: "Carmen Díaz", title: "Radio DJ & Conductora de Colección", photo: "djs/carmen_diaz.jpg" },
+        { start: "23:00", end: "01:00", show: "Baladas Inolvidables", dj: "Emisión Automática", title: "Selección Nocturna de Baladas", photo: "logo.png" }
+    ],
+    3: [ // MIÉRCOLES
+        { start: "01:00", end: "05:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "05:00", end: "11:00", show: "Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "15:00", show: "Fórmula Inolvidable", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "15:00", end: "17:00", show: "Prime Time del Atasco", dj: "José Ibáñez", title: "Radio DJ & Conductor de El Prime Time", photo: "djs/jose_ibanez.jpg" },
+        { start: "17:00", end: "21:00", show: "Fórmula Inolvidable", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "21:00", end: "23:00", show: "Inolvidables de Colección", dj: "Carmen Díaz", title: "Radio DJ & Conductora de Colección", photo: "djs/carmen_diaz.jpg" },
+        { start: "23:00", end: "01:00", show: "Baladas Inolvidables", dj: "Emisión Automática", title: "Selección Nocturna de Baladas", photo: "logo.png" }
+    ],
+    4: [ // JUEVES
+        { start: "01:00", end: "05:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "05:00", end: "11:00", show: "Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "15:00", show: "Fórmula Inolvidable", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "15:00", end: "17:00", show: "Prime Time del Atasco", dj: "José Ibáñez", title: "Radio DJ & Conductor de El Prime Time", photo: "djs/jose_ibanez.jpg" },
+        { start: "17:00", end: "21:00", show: "Fórmula Inolvidable", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "21:00", end: "23:00", show: "Inolvidables de Colección", dj: "Carmen Díaz", title: "Radio DJ & Conductora de Colección", photo: "djs/carmen_diaz.jpg" },
+        { start: "23:00", end: "01:00", show: "Baladas Inolvidables", dj: "Emisión Automática", title: "Selección Nocturna de Baladas", photo: "logo.png" }
+    ],
+    5: [ // VIERNES
+        { start: "01:00", end: "05:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "05:00", end: "11:00", show: "Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "15:00", show: "Fórmula Inolvidable", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "15:00", end: "17:00", show: "Prime Time del Atasco", dj: "José Ibáñez", title: "Radio DJ & Conductor de El Prime Time", photo: "djs/jose_ibanez.jpg" },
+        { start: "17:00", end: "21:00", show: "Fórmula Inolvidable", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "21:00", end: "22:00", show: "Inolvidables de Colección", dj: "Carmen Díaz", title: "Radio DJ & Conductora de Colección", photo: "djs/carmen_diaz.jpg" },
+        { start: "22:00", end: "23:00", show: "Club 958", dj: "Tony Besa", title: "Radio DJ & Especialista Música Llenapistas", photo: "djs/tony_besa.png" },
+        { start: "23:00", end: "01:00", show: "Inolvidables para Bailar", dj: "Emisión Automática", title: "Selección Bailable de Fin de Semana", photo: "logo.png" }
+    ],
+    6: [ // SÁBADO
+        { start: "01:00", end: "07:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "07:00", end: "11:00", show: "Lo Mejor del Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "17:00", show: "Fórmula Weekend", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "17:00", end: "22:00", show: "Fórmula Weekend", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "22:00", end: "23:00", show: "Funkytown", dj: "Jaime Falcón", title: "Radio DJ & Creador de Funkytown", photo: "djs/jaime_falcon.png" },
+        { start: "23:00", end: "01:00", show: "Inolvidables para Bailar", dj: "Emisión Automática", title: "Selección Bailable de Fin de Semana", photo: "logo.png" }
+    ],
+    0: [ // DOMINGO
+        { start: "01:00", end: "07:00", show: "Fórmula Inolvidable Noche", dj: "Emisión Automática", title: "Fórmula Nocturna Inolvidable FM", photo: "logo.png" },
+        { start: "07:00", end: "11:00", show: "Lo Mejor del Morning Inolvidable", dj: "Michael Ramírez", title: "Radio DJ & Conductor del Morning", photo: "djs/michael_ramirez.jpg" },
+        { start: "11:00", end: "15:00", show: "Fórmula Weekend", dj: "José Ibáñez", title: "Radio DJ & Conductor de El Prime Time", photo: "djs/jose_ibanez.jpg" },
+        { start: "15:00", end: "18:00", show: "Fórmula Weekend", dj: "Juan Carlos Santomé", title: "Radio DJ & Coordinador Musical", photo: "djs/juan_carlos_santome.jpg" },
+        { start: "18:00", end: "22:00", show: "Fórmula Weekend", dj: "Jaime Falcón", title: "Radio DJ & Especialista Soul/Funk", photo: "djs/jaime_falcon.png" },
+        { start: "22:00", end: "23:00", show: "Pase Privado", dj: "Xavier Valiño", title: "Periodista & Crítico de Pop y Rock", photo: "djs/xavier_valino.jpg" },
+        { start: "23:00", end: "01:00", show: "Baladas Inolvidables", dj: "Emisión Automática", title: "Selección Nocturna de Baladas", photo: "logo.png" }
+    ]
+};
+
+// Lista general de Radio DJs para navegación manual
+var radioDjs = [
+    {
+        name: "Michael Ramírez",
+        title: "Radio DJ & Conductor del Morning",
+        program: "MORNING INOLVIDABLE",
+        schedule: "Lunes a Viernes • 05:00 a 11:00 h",
+        photo: "djs/michael_ramirez.jpg"
+    },
+    {
+        name: "Juan Carlos Santomé",
+        title: "Radio DJ & Coordinador Musical",
+        program: "FÓRMULA INOLVIDABLE",
+        schedule: "Lunes a Viernes • 11:00 a 15:00 h",
+        photo: "djs/juan_carlos_santome.jpg"
+    },
+    {
+        name: "José Ibáñez",
+        title: "Radio DJ & Conductor de El Prime Time",
+        program: "EL PRIME TIME DEL ATASCO",
+        schedule: "Lunes a Viernes • 15:00 a 17:00 h",
+        photo: "djs/jose_ibanez.jpg"
+    },
+    {
+        name: "Jaime Falcón",
+        title: "Radio DJ & Especialista Soul/Funk",
+        program: "EDICIÓN TARDE & FUNKYTOWN",
+        schedule: "Lunes a Sábado • 17:00 a 21:00 h",
+        photo: "djs/jaime_falcon.png"
+    },
+    {
+        name: "Carmen Díaz",
+        title: "Radio DJ & Conductora de Colección",
+        program: "INOLVIDABLES DE COLECCIÓN",
+        schedule: "Lunes a Viernes • 21:00 a 23:00 h",
+        photo: "djs/carmen_diaz.jpg"
+    },
+    {
+        name: "Tony Besa",
+        title: "Radio DJ & Especialista Música Llenapistas",
+        program: "CLUB 958",
+        schedule: "Viernes • 22:00 a 23:00 h",
+        photo: "djs/tony_besa.png"
+    },
+    {
+        name: "Xavier Valiño",
+        title: "Periodista & Crítico de Pop y Rock",
+        program: "PASE PRIVADO",
+        schedule: "Domingo • 22:00 a 23:00 h",
+        photo: "djs/xavier_valino.jpg"
+    }
 ];
-var songIndex = 0;
+
+/**
+ * Obtener las partes de tiempo oficial en Canarias (Atlantic/Canary)
+ */
+function getCanaryTimeParts() {
+    var now = new Date();
+    try {
+        var dayName = new Intl.DateTimeFormat("en-US", { timeZone: "Atlantic/Canary", weekday: "short" }).format(now);
+        var dayMap = { "Sun": 0, "Mon": 1, "Tue": 2, "Wed": 3, "Thu": 4, "Fri": 5, "Sat": 6 };
+        var day = (dayMap[dayName] !== undefined) ? dayMap[dayName] : now.getDay();
+
+        var timeStr = new Intl.DateTimeFormat("en-US", {
+            timeZone: "Atlantic/Canary",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false
+        }).format(now);
+
+        var parts = timeStr.split(":");
+        var hour = parseInt(parts[0], 10);
+        if (hour === 24) hour = 0;
+        var minute = parseInt(parts[1], 10);
+
+        return { day: day, hour: hour, minute: minute, nowMinutes: hour * 60 + minute };
+    } catch (e) {
+        var h = now.getHours();
+        var m = now.getMinutes();
+        return { day: now.getDay(), hour: h, minute: m, nowMinutes: h * 60 + m };
+    }
+}
+
+/**
+ * Obtener el programa y Radio DJ en directo según la parrilla oficial y la hora exacta del usuario
+ */
+function getLiveProgramAndDj() {
+    var canaryTime = getCanaryTimeParts();
+    var day = canaryTime.day;
+    var nowMinutes = canaryTime.nowMinutes;
+
+    var daySchedule = officialWeeklySchedule[day];
+    if (daySchedule) {
+        for (var i = 0; i < daySchedule.length; i++) {
+            var item = daySchedule[i];
+            var startParts = item.start.split(":");
+            var endParts = item.end.split(":");
+            var startMin = parseInt(startParts[0], 10) * 60 + parseInt(startParts[1], 10);
+            var endMin = parseInt(endParts[0], 10) * 60 + parseInt(endParts[1], 10);
+
+            if (endMin < startMin) {
+                // Rango nocturno cruzando la medianoche (ej. 23:00 a 01:00)
+                if (nowMinutes >= startMin || nowMinutes < endMin) {
+                    return item;
+                }
+            } else if (nowMinutes >= startMin && nowMinutes < endMin) {
+                return item;
+            }
+        }
+    }
+    return daySchedule ? daySchedule[0] : null;
+}
+
+var currentDjIndex = 0;
+var isManualDjOverride = false;
 
 /**
  * Inicialización de la Aplicación HbbTV en Smart TV
@@ -80,8 +254,9 @@ function initHbbtvApp() {
         // 3. Registrar eventos de teclado / mando a distancia
         document.addEventListener("keydown", handleGlobalKeyDown, false);
 
-        // 4. Iniciar rotación de metadatos de emisión en directo
-        setInterval(rotateLiveSongMetadata, 10000);
+        // 4. Iniciar actualización continua de metadatos según el horario en tiempo real
+        updateLiveDjMetadata();
+        setInterval(updateLiveDjMetadata, 10000);
 
     } catch (err) {
         console.warn("[HbbTV Engine] Modo navegador de escritorio (Fallback emulador activo):", err);
@@ -203,10 +378,14 @@ function triggerMenuSelection(index) {
 function selectGreenRadioHd() {
     closeSidebarMenu();
     openPanel("panelRadioHd");
+    updateLiveDjMetadata();
     playHdStreamAudio();
 
-    document.getElementById("audioStatusBadge").innerText = "AUDIO: STREAMING HD 320 Kbps";
-    document.getElementById("audioStatusBadge").style.color = "#4ade80";
+    var statusBadge = document.getElementById("audioStatusBadge");
+    if (statusBadge) {
+        statusBadge.innerText = "AUDIO: STREAMING HD 320 Kbps";
+        statusBadge.style.color = "#4ade80";
+    }
 }
 
 /**
@@ -297,25 +476,79 @@ function restoreTdtAudio() {
         } catch (e) {}
     }
 
-    document.getElementById("audioStatusBadge").innerText = "AUDIO: TDT CONVENCIONAL";
-    document.getElementById("audioStatusBadge").style.color = "#38bdf8";
+    var statusBadge = document.getElementById("audioStatusBadge");
+    if (statusBadge) {
+        statusBadge.innerText = "AUDIO: TDT CONVENCIONAL";
+        statusBadge.style.color = "#38bdf8";
+    }
     alert("Audio restaurado a la señal de Televisión TDT habitual.");
 }
 
 /**
- * Actualizar canción y metadatos en directo
+ * Actualizar foto, nombre, función y programa del Radio DJ según la parrilla oficial
  */
-function rotateLiveSongMetadata() {
-    songIndex = (songIndex + 1) % songPlaylist.length;
-    var currentSong = songPlaylist[songIndex];
-
+function updateLiveDjMetadata() {
+    var programNameEl = document.getElementById("programName");
     var songTitleEl = document.getElementById("songTitle");
     var songArtistEl = document.getElementById("songArtist");
     var songAlbumEl = document.getElementById("songAlbum");
     var coverImgEl = document.getElementById("coverImg");
+    var liveBadgeEl = document.getElementById("liveBadge");
 
-    if (songTitleEl) songTitleEl.innerText = currentSong.title;
-    if (songArtistEl) songArtistEl.innerText = currentSong.artist;
-    if (songAlbumEl) songAlbumEl.innerText = currentSong.album;
-    if (coverImgEl) coverImgEl.src = currentSong.cover;
+    if (!isManualDjOverride) {
+        var liveData = getLiveProgramAndDj();
+        if (liveData) {
+            if (programNameEl) programNameEl.innerText = "PROGRAMA EN DIRECTO: " + liveData.show;
+            if (songTitleEl) songTitleEl.innerText = liveData.dj;
+            if (songArtistEl) songArtistEl.innerText = liveData.title;
+            if (songAlbumEl) songAlbumEl.innerText = liveData.start + " a " + liveData.end + " h (Canarias)";
+            if (liveBadgeEl) liveBadgeEl.innerText = "🔴 EN DIRECTO HD";
+
+            if (coverImgEl) {
+                coverImgEl.style.opacity = "0.3";
+                setTimeout(function() {
+                    coverImgEl.src = liveData.photo;
+                    coverImgEl.alt = liveData.dj + " - " + liveData.show;
+                    coverImgEl.style.opacity = "1";
+                }, 150);
+            }
+            return;
+        }
+    }
+
+    // Modo exploración manual de equipo/voces
+    var dj = radioDjs[currentDjIndex];
+    if (!dj) return;
+
+    if (programNameEl) programNameEl.innerText = "PROGRAMA: " + dj.program;
+    if (songTitleEl) songTitleEl.innerText = dj.name;
+    if (songArtistEl) songArtistEl.innerText = dj.title;
+    if (songAlbumEl) songAlbumEl.innerText = dj.schedule;
+    if (liveBadgeEl) liveBadgeEl.innerText = "👤 VER EQUIPO DJ";
+
+    if (coverImgEl) {
+        coverImgEl.style.opacity = "0.3";
+        setTimeout(function() {
+            coverImgEl.src = dj.photo;
+            coverImgEl.alt = "Foto de " + dj.name + " - Radio DJ Inolvidable FM";
+            coverImgEl.style.opacity = "1";
+        }, 150);
+    }
+}
+
+/**
+ * Rotar manualmente entre los locutores o volver al programa en directo
+ */
+function rotateLiveDj() {
+    isManualDjOverride = true;
+    currentDjIndex = (currentDjIndex + 1) % radioDjs.length;
+    updateLiveDjMetadata();
+}
+
+/**
+ * Volver a sincronizar con el programa en directo
+ */
+function resetToLiveSchedule() {
+    isManualDjOverride = false;
+    updateLiveDjMetadata();
 }
